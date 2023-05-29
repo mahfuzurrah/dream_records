3287e0c95129364eae8c08e83eb45e2b56916cde
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-05-29 13:05:37 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrimaryArtistManage from "./Pages/PrimaryArtistManage";
 import AudioSubmission from "./Pages/AudioSubmission";
 import Earning from "./Pages/Earning";
+import ArtistChannelRequest from "./Pages/ArtistChannelRequest";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<PrimaryArtistManage />} />
           <Route path="a" element={<AudioSubmission />} />
           <Route path="e" element={<Earning />} />
+          <Route path="ar" element={<ArtistChannelRequest />} />
         </Routes>
       </Router>
     </div>
