609b9fab38f6828c428600f229e5495adf23c2e3
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-05-29 12:24:09 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrimaryArtistManage from "./Pages/PrimaryArtistManage";
 
 function App() {
   return (
     <div>
       <Router>
-      <Route path="/" element={<PrimaryArtistManage />} />
-    </Router>
+        <Routes>
+          <Route path="/" element={<PrimaryArtistManage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
