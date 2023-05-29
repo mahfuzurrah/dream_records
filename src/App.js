f49827e99773be3bdc90d95a27fa72486dda45c8
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-05-29 11:48:34 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from './Component/Layout/MainLayout';
-import Dashboard from "../../../dream_records_dashboard/src/Pages/Dashboard";
+import Dashboard from './Pages/Dashboard';
 function App() {
   return (
     <>
