a703518bb0ec0363bf42163873f9e26ab9496b4c
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-05-29 12:46:43 +0600

    ADD

--- src/Pages/EditPrimaryArtist.js
+++ src/Pages/EditPrimaryArtist.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ImageUploadForm from "../Component/ImageUpload/ImageUploadForm";
 import InputField from "../Component/InputField/InputField";
@@ -7,14 +7,39 @@ function EditPrimaryArtist() {
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    navigate("primary_artist_manage");
+    navigate("/primary_artist_manage");
   };
 
-    const [name, setName] = useState('');
-  
-    const handleNameChange = (event) => {
-      setName(event.target.value);
-    };
+  const [fullName, setFullName] = useState("");
+  const [instagramId, setInstagramId] = useState("");
+  const [spotifyId, setSpotifyId] = useState("");
+  const [appleId, setAppleId] = useState("");
+  const [facebookUrl, setFacebookUrl] = useState("");
+  const [youtubeUrl, setYoutubeUrl] = useState("");
+
+  const handleNameChange = (event) => {
+    setFullName(event.target.value);
+  };
+
+  const handleInstagramIdChange = (event) => {
+    setInstagramId(event.target.value);
+  };
+
+  const handleSpotifyIdChange = (event) => {
+    setSpotifyId(event.target.value);
+  };
+
+  const handleAppleIdChange = (event) => {
+    setAppleId(event.target.value);
+  };
+
+  const handleFacebookUrlChange = (event) => {
+    setFacebookUrl(event.target.value);
+  };
+
+  const handleYoutubeUrlChange = (event) => {
+    setYoutubeUrl(event.target.value);
+  };
 
   return (
     <div>
@@ -40,16 +65,40 @@ function EditPrimaryArtist() {
             </div>
             <div className="input_group mt-4">
               <div className="item">
-              <InputField label="Full Name" value={name} onChange={handleNameChange}/>
-              <InputField label="Instagram Id" value={name} onChange={handleNameChange}/>
+                <InputField
+                  label="Full Name"
+                  value={fullName}
+                  onChange={handleNameChange}
+                />
+                <InputField
+                  label="Instagram Id"
+                  value={instagramId}
+                  onChange={handleInstagramIdChange}
+                />
               </div>
               <div className="item">
-              <InputField label="Spotify Id" value={name} onChange={handleNameChange}/>
-              <InputField label="Apple Id" value={name} onChange={handleNameChange}/>
+                <InputField
+                  label="Spotify Id"
+                  value={spotifyId}
+                  onChange={handleSpotifyIdChange}
+                />
+                <InputField
+                  label="Apple Id"
+                  value={appleId}
+                  onChange={handleAppleIdChange}
+                />
               </div>
               <div className="item">
-              <InputField label="Facebook URL" value={name} onChange={handleNameChange}/>
-              <InputField label="YouTube Channel URL" value={name} onChange={handleNameChange}/>
+                <InputField
+                  label="Facebook URL"
+                  value={facebookUrl}
+                  onChange={handleFacebookUrlChange}
+                />
+                <InputField
+                  label="YouTube Channel URL"
+                  value={youtubeUrl}
+                  onChange={handleYoutubeUrlChange}
+                />
               </div>
             </div>
           </div>
