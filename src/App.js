1c22d73f469f2a532c4d637d913a673020595d67
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-05-29 13:51:34 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,74 +1,38 @@
-import {
-  MenuFoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
-import { useState } from 'react';
-const { Header, Sider, Content } = Layout;
-const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrimaryArtistManage from "./Pages/PrimaryArtistManage";
+import AudioSubmission from "./Pages/AudioSubmission";
+import Earning from "./Pages/Earning";
+import ArtistChannelRequest from "./Pages/ArtistChannelRequest";
+import ContentIdRequest from "./Pages/ContentIdRequest";
+import AddClaimRelease from "./Pages/AddClaimRelease";
+import Analytics from './Pages/Analytics';
+import Catalog from './Pages/Catalog';
+import Dashboard from './Pages/Dashboard';
+import ReleaseAudio from './Pages/ReleaseAudio';
+import MainLayout from "./Component/Layout/MainLayout";
+
+function App() {
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="release-audio" element={<ReleaseAudio />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="primary_artist_manage" element={<PrimaryArtistManage />} />
+          <Route path="add_claim_release" element={<AddClaimRelease />} />
+          <Route path="content_id_request" element={<ContentIdRequest />} />
+          <Route path="artist_channel_request" element={<ArtistChannelRequest />} />
+          <Route path="earning" element={<Earning />} />
+          <Route path="audio_submission" element={<AudioSubmission />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
-};
+}
+
 export default App;
